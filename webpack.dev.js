071d44f6e22558cb7d7ea1bb49692f923aa6f360
yangguowei <yangguowei@kuaizi.co>
2019-10-29 10:16:04 +0800

    测试dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,6 +11,29 @@ module.exports = merge(baseConfig, {
     port: 6060,
     hot: true,
   },
+  devServer: {
+    clientLogLevel: 'warning',
+    disableHostCheck: true,
+    hot: true,
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+    compress: true,
+    host: '0.0.0.0',
+    port: 6060,
+    open: true,
+    overlay:  {
+        warnings: false,
+        errors: true
+    },
+    // publicPath: config.dev.assetsPublicPath,
+    proxy: {
+    },
+    quiet: true,
+    watchOptions: {
+      poll: false,
+      ignored: /\/node_modules\/.*/
+    }
+  },
   module: {
     rules: []
   },
